26c17edddff17bbd19b3c8bf6b61f75f8881514f
jRimbault <jacques.rimbault@gmail.com>
2018-10-10 19:46:24 +0200

    for codeblocks for windows

---
+++ source/configuration.h
@@ -0,0 +1,4 @@
+#define MYSQL_HOST "localhost"
+#define MYSQL_USER "root"
+#define MYSQL_PWD ""
+#define MYSQL_DB "piscine"
--- source/main.c
+++ source/main.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "../include/mysql.h"
+#include "configuration.h"
 
 
 int main(int argc, char** argv)
 {
-    for (int i = 0; i < argc; i += 1) {
-        printf("%s\n", argv[i]);
+    MYSQL* mysql = NULL;
+
+    mysql = mysql_init(mysql);
+
+    if (!mysql) {
+        puts("Init faild, out of memory?");
+        return EXIT_FAILURE;
     }
-    return 0;
+
+    if (!mysql_real_connect(
+        mysql,       /* MYSQL structure to use */
+        MYSQL_HOST,  /* server hostname or IP address */
+        MYSQL_USER,  /* mysql user */
+        MYSQL_PWD,   /* password */
+        MYSQL_DB,    /* default database to use, NULL for none */
+        0,           /* port number, 0 for default */
+        NULL,        /* socket file or named pipe name */
+        CLIENT_FOUND_ROWS /* connection flags */ )) {
+        puts("Connect failed\n");
+    } else {
+        puts("Connect OK\n");
+    }
+
+    mysql_close(mysql);
+
+    return EXIT_SUCCESS;
 }
